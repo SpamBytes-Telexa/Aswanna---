efe0fc20b35cc21d9f309076e141a894f56cc9a3
mickymarsh <162584880+mickymarsh@users.noreply.github.com>
2025-07-04 17:23:08 +0530

    Home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes, RouterProvider, createBrowserRouter, Outlet, Navigate } from 'react-router-dom';
+
 import FarmerForm from './Components/Blockchain_Contracts/farmer_from';
+import Home from './madhuni/pages/Home';
+
+
 
 const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/farmerform",
+      element: <FarmerForm />,
+    }
+  ])
+
   return (
-    <div>
-     <FarmerForm />
-    </div>
-  )
+        <RouterProvider router={router} />
+  );
 }
 
 export default App
