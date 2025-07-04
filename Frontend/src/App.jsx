04e93cd7826380f4a874c5ac41e49df1dee2b2ce
Semini-Sawbhagya <161110096+Semini-Sawbhagya@users.noreply.github.com>
2025-07-04 20:52:22 +0530

    Revert "updated"
    This reverts commit c34826da05c013fc65b43af213433fc0616d9279.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
 import FarmerForm from './Components/Blockchain_Contracts/farmer_from';
-import Farmer_contracts from './Components/Blockchain_Contracts/farmer_contracts';
-import BuyerUI from './Components/Blockchain_Contracts/Buyer_UI';
-import OfferDetails from './Components/Blockchain_Contracts/offer_details';
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 const App = () => {
   return (
     <div>
-      <Router>
-        <Routes>
-        <Route path="/" element={<FarmerForm />} />
-        <Route path="/contracts" element={<Farmer_contracts />} />
-        <Route path="/buyer" element={<BuyerUI/>} />
-        <Route path="/offer/:id" element={<OfferDetails />} />
-        </Routes>
-      </Router> 
+     <FarmerForm />
     </div>
   )
 }
