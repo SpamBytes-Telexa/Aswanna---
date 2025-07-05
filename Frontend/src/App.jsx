8cad7f270dfe88833ed85461dbae5239189ed328
mickymarsh <162584880+mickymarsh@users.noreply.github.com>
2025-07-05 21:25:24 +0530

    chatbot updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,26 +1,21 @@
 
 import React from 'react'
 
-import FarmerForm from './Components/Blockchain_Contracts/farmer_from';
-import Farmer_contracts from './Components/Blockchain_Contracts/farmer_contracts';
-import BuyerUI from './Components/Blockchain_Contracts/Buyer_UI';
-import OfferDetails from './Components/Blockchain_Contracts/offer_details';
-import Chatbot from './madhuni/pages/Chatbot';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './madhuni/pages/Home';
-
-
-import FarmerForm from "./Components/Blockchain_Contracts/farmer_from";
-import Farmer_contracts from "./Components/Blockchain_Contracts/farmer_contracts";
-import BuyerUI from "./Components/Blockchain_Contracts/Buyer_UI";
-import OfferDetails from "./Components/Blockchain_Contracts/offer_details";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   useNavigate,
 } from "react-router-dom";
+
+import FarmerForm from './Components/Blockchain_Contracts/farmer_from';
+import Farmer_contracts from './Components/Blockchain_Contracts/farmer_contracts';
+import BuyerUI from './Components/Blockchain_Contracts/Buyer_UI';
+import OfferDetails from './Components/Blockchain_Contracts/offer_details';
+
+import Chatbot from './madhuni/pages/Chatbot';
 import Home from "./madhuni/pages/Home";
+
 import CropRecommendationForm from "./Pages/cropRecommendationForm";
 import RecommendationResults from "./Pages/recommendationResult";
 
