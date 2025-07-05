0ee7bff357b6b7e2a1ca67df03501efd157cd077
Navodi S.Y.A.C. <syacnavodi420@gmail.com>
2025-07-05 13:22:22 +0530

    cropRecommendation Frontend completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,30 +1,49 @@
-import React from 'react'
+import React from "react";
 
-import FarmerForm from './Components/Blockchain_Contracts/farmer_from';
-import Farmer_contracts from './Components/Blockchain_Contracts/farmer_contracts';
-import BuyerUI from './Components/Blockchain_Contracts/Buyer_UI';
-import OfferDetails from './Components/Blockchain_Contracts/offer_details';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './madhuni/pages/Home';
-
+import FarmerForm from "./Components/Blockchain_Contracts/farmer_from";
+import Farmer_contracts from "./Components/Blockchain_Contracts/farmer_contracts";
+import BuyerUI from "./Components/Blockchain_Contracts/Buyer_UI";
+import OfferDetails from "./Components/Blockchain_Contracts/offer_details";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
+import Home from "./madhuni/pages/Home";
+import CropRecommendationForm from "./Pages/cropRecommendationForm";
+import RecommendationResults from "./Pages/recommendationResult";
 
 const App = () => {
+  const CropRecommendationFormWithNav = () => {
+    const navigate = useNavigate();
+    const handleSubmit = (formData) => {
+      navigate("/recommendation-results", { state: { formData } });
+    };
+    return <CropRecommendationForm onSubmit={handleSubmit} />;
+  };
 
   return (
-
     <div>
       <Router>
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Home />} />
           <Route path="/farmerform" element={<FarmerForm />} />
           <Route path="/contracts" element={<Farmer_contracts />} />
           <Route path="/buyer" element={<BuyerUI />} />
           <Route path="/offer/:id" element={<OfferDetails />} />
+          <Route
+            path="/crop-recommendation"
+            element={<CropRecommendationFormWithNav />}
+          />
+          <Route
+            path="/recommendation-results"
+            element={<RecommendationResults />}
+          />
         </Routes>
       </Router>
     </div>
-
   );
-}
+};
 
 export default App;
