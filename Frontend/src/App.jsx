625683a5bf02005bfa160c3f0688ef3f7c4e4c9e
mickymarsh <162584880+mickymarsh@users.noreply.github.com>
2025-07-05 10:28:35 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, RouterProvider, createBrowserRouter, Outlet, Navigate } from 'react-router-dom';
 
 import FarmerForm from './Components/Blockchain_Contracts/farmer_from';
 import Farmer_contracts from './Components/Blockchain_Contracts/farmer_contracts';
