b6c4ca44131538371ff8ecdc4bed29ddc0278a86
Semini-Sawbhagya <161110096+Semini-Sawbhagya@users.noreply.github.com>
2025-07-07 20:37:17 +0530

    Create login.py

---
+++ Backend/routers/login.py
@@ -0,0 +1,57 @@
+from fastapi import APIRouter, Depends, HTTPException,Response,status
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+from uuid import UUID
+from datetime import date
+from database import engine, get_db
+from models import Base, User, UserBase,UserLogin
+from fastapi.middleware.cors import CORSMiddleware
+from Security.utils import hash_password,verify_password
+from Security.security import create_access_token
+
+router=APIRouter();
+@router.post("/register")
+def register_user(user: UserBase, db: Session = Depends(get_db)):
+    try:
+        # Hash the password before storing
+        hashed_password = hash_password(user.password_hash)
+
+        # Prepare and execute the PostgreSQL stored procedure call
+        query = text("CALL db_create_account(:wallet_address, :username, :email, :password_hash, :role)")
+        db.execute(query, {
+            "wallet_address": user.wallet_address,
+            "username": user.user_name,
+            "email": user.email,
+            "password_hash": hashed_password,
+            "role": user.role
+        })
+
+
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=f"Registration failed: {str(e)}")
+
+    return {"message": "User profile registered successfully"}
+
+@router.post("/login")
+def login_user(user: UserLogin, response: Response, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user or not verify_password(user.password , db_user.password_hash):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+
+    access_token = create_access_token({
+        "sub": db_user.name,
+        "role": db_user.role,
+        "user_id": str(db_user.id)
+    })
+
+    return {
+        "message": "Login Success",
+        "access_token": access_token,
+        "token_type": "bearer",
+        "user_id": db_user.id,
+        "role": db_user.role,
+        "user_name": db_user.name
+    }
