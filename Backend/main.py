b87d123851e0e7cf98458bda9eaf507e10b02b66
Semini-Sawbhagya <161110096+Semini-Sawbhagya@users.noreply.github.com>
2025-07-07 20:36:25 +0530

    updated main.py

--- Backend/main.py
+++ Backend/main.py
@@ -4,9 +4,11 @@ from fastapi import FastAPI, Depends
 #from database import engine, get_db
 #from models import Base, CropOffer
 from fastapi.middleware.cors import CORSMiddleware
-from routers import predict
+#from routers import predict
 
-from maduni.routes import chat
+#from maduni.routes import chat
+from routers import contracts
+from routers import login
 
 #from routers import contracts;    
 app = FastAPI()
@@ -32,34 +34,8 @@ def read_root():
     return {"message": "Backend running ✅"}
 
 
-app.include_router(chat.router, prefix='/chatbot', tags=['Chatbot'])
-
-'''
-@app.get("/api/my-contracts")
-def get_my_contracts(db: Session = Depends(get_db)):
-    # Hardcoded UUID string
-    hardcoded_farmer_id = UUID("1cdfb24f-a902-4bac-825a-074973263a3d")
-    
-    offers = db.query(CropOffer).filter(CropOffer.farmer_id == hardcoded_farmer_id).all()
-
-    contracts = []
-    for offer in offers:
-        contracts.append({
-            "id": str(offer.id),
-            "product": offer.product,
-            "price": offer.price,
-            "status": offer.status,
-            "delivery_method": offer.delivery_method,
-            "location": offer.location,
-            "quantity": offer.quantity, 
-            "deadline": offer.deadline.isoformat() if offer.deadline else None,
-            "photo": offer.photo,   
-            
-        })
-    return {"contracts": contracts}
-
-'''
-
+#app.include_router(chat.router, prefix='/chatbot', tags=['Chatbot'])
 app.include_router(contracts.router, prefix="/blockchain", tags=["contracts"])
-app.include_router(predict.router, prefix="/ml", tags=["predict"])
+app.include_router(login.router, prefix="/auth", tags=["login"])
+#app.include_router(predict.router, prefix="/ml", tags=["predict"])
 
